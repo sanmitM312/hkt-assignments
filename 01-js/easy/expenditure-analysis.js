86222c0852747f553951ced2894c66a2f3d3061a
sanmitM312 <iloveexploits1721@gmail.com>
2024-01-12 17:32:29 +0530

    01-js easy done

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -14,7 +14,70 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  // create a map <category, priceSpent>
+  // iterate transactions and continue adding the priceSpent
+  // iterate through the map and create an object for each category and add to the list
+  const op = []
+  const categories = new Set();
+  for(let transaction of transactions){
+    categories.add(transaction.category)
+  }
+
+  for(let cat of categories){
+    op.push({category : cat, totalSpent : 0})
+  }
+  for(let cat of categories){
+    for(let i = 0 ; i < transactions.length; i++){
+      if(transactions[i]["category"] == cat){
+        for(let k = 0; k < op.length; k++){
+          if(op[k]["category"] == cat){
+            op[k]["totalSpent"] +=  transactions[i]["price"]
+          }
+        }
+      }
+    }
+  }
+  
+  // console.log(op)
+  return op
 }
 
+const obj = [
+  {
+    id: 1,
+    timestamp: 1656076800000,
+    price: 10,
+    category: 'Food',
+    itemName: 'Pizza',
+  },
+  {
+    id: 2,
+    timestamp: 1656259600000,
+    price: 20,
+    category: 'Food',
+    itemName: 'Burger',
+  },
+  {
+    id: 3,
+    timestamp: 1656019200000,
+    price: 15,
+    category: 'Clothing',
+    itemName: 'T-Shirt',
+  },
+  {
+    id: 4,
+    timestamp: 1656364800000,
+    price: 30,
+    category: 'Electronics',
+    itemName: 'Headphones',
+  },
+  {
+    id: 5,
+    timestamp: 1656105600000,
+    price: 25,
+    category: 'Clothing',
+    itemName: 'Jeans',
+  },
+]
+calculateTotalSpentByCategory(obj)
 module.exports = calculateTotalSpentByCategory;
--- 01-js/easy/findLargestElement.js
+++ 01-js/easy/findLargestElement.js
@@ -3,10 +3,17 @@
   Example:
   - Input: [3, 7, 2, 9, 1]
   - Output: 9
+  Also for windows users; to run tests it is "npx jest ./tests/findLargestElement.test.js" not with backslash path
 */
 
 function findLargestElement(numbers) {
-    
+    let largestNum = numbers[0];
+    for(let i=0; i < numbers.length; i++){
+        if(numbers[i] > largestNum){
+            largestNum = numbers[i];
+        }
+    }
+    return largestNum;
 }
 
 module.exports = findLargestElement;
